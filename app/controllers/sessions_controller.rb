98be16037ea8ab27141b60d4dacbe09775afbdee
unknown <mohamed.hany556@gmail.com>
2021-02-16 00:23:22 +0200

    login&logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,31 @@
 class SessionsController < ApplicationController
 
 
-    #login route
+    #show login form
     def new 
         
+    end
+
+    #for submiting login form
+    def create
+        #check for username in database
+        user = User.find_by(username: params[:session][:username])
+        
+        #checks if the user is found and the password is correct
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id]= user.id
+            flash[:success] = "You have successfully logged in"
+            redirect_to root_path
+        else
+            flash.now[:error] = "Username or password is wrong"
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:uer_id] = nil
+        flash[:success] = "You have successfully logged out"
+        redirect_to login_path
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,17 @@ Rails.application.routes.draw do
   #Root Route
   root 'chatroom#index'
  
-  #Login Route
+  #Show login form
   get 'login', to: 'sessions#new'
 
+  #submit login form  
+  post 'login', to: 'sessions#create'
+  
+  #logout
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users
 
   resources :messages
-  
+
 end
